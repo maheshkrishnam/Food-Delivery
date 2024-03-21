07d19831eacd0bbb2f3f952214a48ff0789c3392
Mahesh Krishnam <131548711+maheshkrishnam@users.noreply.github.com>
2024-03-21 15:34:30 +0530

    started the practice project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <Navbar/>
+      </div>
+    </>
+  )
+}
+
+export default App
